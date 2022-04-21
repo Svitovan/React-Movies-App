d46d61541dece3679edfd46f73363640b1feb37e
WorkPC <svitovan@outlook.com>
2022-04-21 17:13:06 +0200

    +

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,19 +3,48 @@ import React from "react";
 class Search extends React.Component {
     state = {
         search: '',
+        type: 'all'
     }
+
+    handleKey = (event) => {
+        if(event.key === 'Enter') {
+            this.props.searchMovies(this.state.search);
+        }
+    }
+
+   handleFilter = (event) => {
+
+
+   }
 
     render() {
         return <div className="row">
             <div className="col s12">
-            <div className="input-field inline">
+            <div className="input-field  ">
                 <input 
                     placeholder="search" 
                     type="search" 
                     className="validate" 
                     value={this.state.search}
-                    
+                    onChange={(e)=> this.setState({search: e.target.value})}
+                    onKeyDown={this.handleKey}
                 />
+                <button className="btn" onClick={() => this.props.searchMovies(this.state.search)}>Search</button>
+                
+            </div>
+            <div>
+                <label>
+                    <input class="with-gap" name="type" type="radio" data-type="all" />
+                    <span>All</span>
+                </label>
+                <label>
+                    <input class="with-gap" name="type" type="radio" data-type="movie" />
+                    <span>Movies</span>
+                </label>
+                <label>
+                    <input class="with-gap" name="type" type="radio" data-type="series" />
+                    <span>Series</span>
+                </label>
             </div>
             </div>
         </div>
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {Movies} from '../components/Movies';
 import {Preloader} from '../components/Preloader';
+import {Search} from '../components/Search';
 
 class Main extends React.Component {
   state = {
@@ -8,7 +9,13 @@ class Main extends React.Component {
   }
 
   componentDidMount() {
-    fetch('http://www.omdbapi.com/?apikey=73590118&s=Matrix')
+    fetch('http://www.omdbapi.com/?apikey=73590118&s=2012')
+      .then(response => response.json())
+      .then(data => this.setState({movies: data.Search})) 
+  }
+
+  searchMovies = (str) => {
+    fetch(`http://www.omdbapi.com/?apikey=73590118&s=${str}`)
       .then(response => response.json())
       .then(data => this.setState({movies: data.Search})) 
   }
@@ -17,7 +24,8 @@ class Main extends React.Component {
     const {movies} = this.state;
 
     return <main className="container content">
-       {
+      <Search searchMovies={this.searchMovies}/>
+       {   
          movies.length ? (
             <Movies movies={this.state.movies}/>
          ) : <Preloader />
