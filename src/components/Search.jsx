09e6810577e44700e48878242994c33f1f02272c
Svitovan <svitovan@outlook.com>
2022-04-22 19:34:43 +0200

    finish

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,55 +1,82 @@
 import React from "react";
 
 class Search extends React.Component {
-    state = {
-        search: '',
-        type: 'all'
+  state = {
+    search: "",
+    type: "all",
+  };
+
+  handleKey = (event) => {
+    if (event.key === "Enter") {
+      this.props.searchMovies(this.state.search, this.state.type);
     }
+  };
 
-    handleKey = (event) => {
-        if(event.key === 'Enter') {
-            this.props.searchMovies(this.state.search);
-        }
-    }
+  handleFilter = (event) => {
+    this.setState(()=>({type: event.target.dataset.type}),()=>{
+      this.props.searchMovies(this.state.search, this.state.type);
+    });
+  };
 
-   handleFilter = (event) => {
-
-
-   }
-
-    render() {
-        return <div className="row">
-            <div className="col s12">
-            <div className="input-field  ">
-                <input 
-                    placeholder="search" 
-                    type="search" 
-                    className="validate" 
-                    value={this.state.search}
-                    onChange={(e)=> this.setState({search: e.target.value})}
-                    onKeyDown={this.handleKey}
-                />
-                <button className="btn" onClick={() => this.props.searchMovies(this.state.search)}>Search</button>
-                
-            </div>
-            <div>
-                <label>
-                    <input class="with-gap" name="type" type="radio" data-type="all" />
-                    <span>All</span>
-                </label>
-                <label>
-                    <input class="with-gap" name="type" type="radio" data-type="movie" />
-                    <span>Movies</span>
-                </label>
-                <label>
-                    <input class="with-gap" name="type" type="radio" data-type="series" />
-                    <span>Series</span>
-                </label>
-            </div>
-            </div>
+  render() {
+    return (
+      <div className="row">
+        <div className="col s12">
+          <div className="input-field  ">
+            <input
+              placeholder="search"
+              type="search"
+              className="validate"
+              value={this.state.search}
+              onChange={(e) => this.setState({ search: e.target.value })}
+              onKeyDown={this.handleKey}
+            />
+            <button
+              className="btn"
+              onClick={() => this.props.searchMovies(this.state.search, this.state.type)}
+            >
+              Search
+            </button>
+          </div>
+          <div>
+            <label>
+              <input
+                className="with-gap"
+                name="type"
+                type="radio"
+                data-type="all"
+                onChange={this.handleFilter}
+                checked={this.state.type === 'all'}
+              />
+              <span>All</span>
+            </label>
+            <label>
+              <input
+                className="with-gap"
+                name="type"
+                type="radio"
+                data-type="movie"
+                onChange={this.handleFilter}
+                checked={this.state.type === 'movie'}
+              />
+              <span>Movies</span>
+            </label>
+            <label>
+              <input
+                className="with-gap"
+                name="type"
+                type="radio"
+                data-type="series"
+                onChange={this.handleFilter}
+                checked={this.state.type === 'series'}
+              />
+              <span>Series</span>
+            </label>
+          </div>
         </div>
-    }
-
+      </div>
+    );
+  }
 }
 
-export {Search};
+export { Search };
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,37 +1,41 @@
 import React from "react";
-import {Movies} from '../components/Movies';
-import {Preloader} from '../components/Preloader';
-import {Search} from '../components/Search';
+import { Movies } from "../components/Movies";
+import { Preloader } from "../components/Preloader";
+import { Search } from "../components/Search";
 
 class Main extends React.Component {
   state = {
     movies: [],
-  }
+    loading: true
+  };
 
   componentDidMount() {
-    fetch('http://www.omdbapi.com/?apikey=73590118&s=2012')
-      .then(response => response.json())
-      .then(data => this.setState({movies: data.Search})) 
+    fetch("http://www.omdbapi.com/?apikey=73590118&s=2012")
+      .then((response) => response.json())
+      .then((data) => this.setState({ movies: data.Search, loading: false }));
   }
 
-  searchMovies = (str) => {
-    fetch(`http://www.omdbapi.com/?apikey=73590118&s=${str}`)
-      .then(response => response.json())
-      .then(data => this.setState({movies: data.Search})) 
-  }
+  searchMovies = (str, type = "all") => {
+    this.setState({laoding: true})
+    fetch(
+      `http://www.omdbapi.com/?apikey=73590118&s=${str}${
+        type !== "all" ? `&type=${type}` : ""
+      }`
+    )
+      .then((response) => response.json())
+      .then((data) => this.setState({ movies: data.Search, loading: false }));
+  };
 
   render() {
-    const {movies} = this.state;
+    const { movies, loading } = this.state;
 
-    return <main className="container content">
-      <Search searchMovies={this.searchMovies}/>
-       {   
-         movies.length ? (
-            <Movies movies={this.state.movies}/>
-         ) : <Preloader />
-       }
-    </main>
+    return (
+      <main className="container content">  
+        <Search searchMovies={this.searchMovies} />
+        {loading ? (<Preloader />) : (<Movies movies={movies} />)}
+      </main>
+    );
   }
 }
 
-export {Main}
+export { Main };
